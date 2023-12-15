15942860c82184bf8348271e9980f8f5d0647bc1
meteor-01 <1327564517@qq.com>
2023-12-15 19:00:41 +0800

    删除代理模式的测试类中无用的类

--- proxy/src/test/java/com/meteor/design/AppTest.java
+++ proxy/src/test/java/com/meteor/design/AppTest.java
@@ -7,48 +7,42 @@ import com.meteor.design.service.UserService;
 import com.meteor.design.static_proxy.StaticProxy;
 import org.junit.Test;
 
-import javax.jws.soap.SOAPBinding;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-
 /*
-* author: meteor_01
-* date: 2023/10/31
-* desc: 测试
-**/
-public class AppTest 
-{
+ * author: meteor_01
+ * date: 2023/10/31
+ * desc: 测试
+ **/
+public class AppTest {
     /*
-    * author: meteor_01
-    * date: 2023/10/31
-    * desc: 测试静态代理
-    **/
+     * author: meteor_01
+     * date: 2023/10/31
+     * desc: 测试静态代理
+     **/
     @Test
-    public void testStaticProxy(){
+    public void testStaticProxy() {
         StaticProxy userServiceProxy = new StaticProxy(new UserService());
         userServiceProxy.queryUserInfo();
     }
 
     /*
-    * author: meteor_01
-    * date: 2023/11/1
-    * desc: 测试JDK动态代理
-    **/
+     * author: meteor_01
+     * date: 2023/11/1
+     * desc: 测试JDK动态代理
+     **/
     @Test
-    public void testJDKProxy(){
-        IUserService proxy = (IUserService)JDKProxyFactory.getProxy(new UserService());
+    public void testJDKProxy() {
+        IUserService proxy = (IUserService) JDKProxyFactory.getProxy(new UserService());
         proxy.queryUserInfo();
     }
 
     /*
-    * author: meteor_01
-    * date: 2023/11/1
-    * desc: 测试cglib动态代理
-    **/
+     * author: meteor_01
+     * date: 2023/11/1
+     * desc: 测试cglib动态代理
+     **/
     @Test
-    public void testCGLIBProxy(){
-        UserService proxy = (UserService)CGLIBProxyFactory.getProxy(UserService.class);
+    public void testCGLIBProxy() {
+        UserService proxy = (UserService) CGLIBProxyFactory.getProxy(UserService.class);
         proxy.queryUserInfo();
     }
 
